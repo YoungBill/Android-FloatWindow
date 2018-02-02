479599e65cf7fd85c5a80df0f3e6445a5618398b
taochen <taochen@baina.com>
2018-02-02 20:47:42 +0800

    [feature] MainActivity添加Window,PhoneWindow,DecorView相关

--- sample/src/main/java/com/baina/floatwindow/MainActivity.java
+++ sample/src/main/java/com/baina/floatwindow/MainActivity.java
@@ -3,16 +3,43 @@ package com.baina.floatwindow;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 
 
 public class MainActivity extends Activity {
 
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Window window = getWindow();
+        Log.d(TAG, "onCreate,window == null?" + window == null ? "yes" : "false");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Window window = getWindow();
+        Log.d(TAG, "onPause,window == null?" + window == null ? "yes" : "false");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Window window = getWindow();
+        Log.d(TAG, "onStop,window == null?" + window == null ? "yes" : "false");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Window window = getWindow();
+        Log.d(TAG, "onDestroy,window == null?" + window == null ? "yes" : "false");
     }
 
     public void OnClick(View view) {
